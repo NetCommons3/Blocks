634fd58352c41f43df9ee7f2950e7258190521ff
s-nakajima <nakajimashouhei@gmail.com>
2015-09-06 09:14:30 +0900

    Migrationの修正

--- Config/Migration/1434983278_init.php
+++ Config/Migration/1434983278_init.php
@@ -53,7 +53,7 @@ e.g.) create_content,  post_top_article', 'charset' => 'utf8'),
 					'room_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
 					'plugin_key' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'block key |  プラグインKEY | plugins.key | ', 'charset' => 'utf8'),
 					'key' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'Key of the block.', 'charset' => 'utf8'),
-					'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'Name of the block.', 'charset' => 'utf8'),
+					'name' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'Name of the block.', 'charset' => 'utf8'),
 					'public_type' => array('type' => 'integer', 'null' => false, 'default' => '1', 'length' => 4, 'unsigned' => false, 'comment' => '一般以下のパートが閲覧可能かどうか。
 （0:非公開, 1:公開, 2:期間限定公開）
 
@@ -63,9 +63,9 @@ e.g.) ルーム管理者、またはそれに準ずるユーザ: ルーム管理
 期間限定公開の場合、現在時刻がfrom-toカラムの範囲内の時に公開。'),
 					'from' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => 'Datetime display frame from.'),
 					'to' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => 'Datetime display frame to.'),
-					'translation_engine' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'is_auto_translated' => array('type' => 'boolean', 'null' => false, 'default' => null),
-					'is_first_auto_translation' => array('type' => 'boolean', 'null' => false, 'default' => null),
+					'translation_engine' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'is_auto_translated' => array('type' => 'boolean', 'null' => true, 'default' => null),
+					'is_first_auto_translation' => array('type' => 'boolean', 'null' => true, 'default' => null),
 					'created_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 					'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 					'modified_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
