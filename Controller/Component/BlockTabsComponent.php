223afbc4eee6e3eb0de7813bf5726e018de06c0f
s-nakajima <nakajimashouhei@gmail.com>
2015-09-15 19:24:45 +0900

    ブロック編集タブの共通化

--- Controller/Component/BlockTabsComponent.php
+++ Controller/Component/BlockTabsComponent.php
@@ -24,14 +24,16 @@ class BlockTabsComponent extends Component {
  * @var string
  */
 	const MAIN_TAB_BLOCK_INDEX = 'block_index',
-			MAIN_TAB_FRAME_SETTING = 'frame_settings';
+			MAIN_TAB_FRAME_SETTING = 'frame_settings',
+			MAIN_TAB_PERMISSION = 'role_permissions';
 
 /**
  * Const of main tab
  *
  * @var string
  */
-	const BLOCK_TAB_SETTING = 'block_settings';
+	const BLOCK_TAB_SETTING = 'block_settings',
+			BLOCK_TAB_PERMISSION = 'role_permissions';
 
 /**
  * Called after the Controller::beforeFilter() and before the controller action
@@ -60,6 +62,23 @@ class BlockTabsComponent extends Component {
 			return;
 		}
 
+		$this->__setMainTabs($controller);
+
+		//ブロック設定タブ
+		if (! isset($this->settings['blockTabs'])) {
+			return;
+		}
+
+		$this->__setBlockTabs($controller);
+	}
+
+/**
+ * Set main tabs
+ *
+ * @param Controller $controller Controller with components to beforeRender
+ * @return void
+ */
+	private function __setMainTabs(Controller $controller) {
 		$defaultUrls = array(
 			self::MAIN_TAB_BLOCK_INDEX => array(
 				'plugin' => $controller->params['plugin'],
@@ -73,24 +92,51 @@ class BlockTabsComponent extends Component {
 				'action' => 'edit',
 				'frame_id' => Current::read('Frame.id'),
 			),
+			self::MAIN_TAB_PERMISSION => array(
+				'plugin' => $controller->params['plugin'],
+				'controller' => Inflector::singularize($controller->params['plugin']) . '_block_role_permissions',
+				'action' => 'edit',
+				'frame_id' => Current::read('Frame.id'),
+				'block_id' => Current::read('Block.id'),
+			),
 		);
 
 		$settings = array();
 		foreach ($this->settings['mainTabs'] as $key => $tab) {
-			if (in_array($tab, $this->settings['mainTabs'])) {
+			if (! is_array($tab) && in_array($tab, $this->settings['mainTabs'], true)) {
 				$this->settings['mainTabs'][$tab] = array();
 				$key = $tab;
 			}
 
-			if (isset($this->settings['mainTabs'][$key])) {
-				$settings[$key] = $this->settings['mainTabs'][$key];
-				if (! isset($this->settings['mainTabs'][$key]['url']) && isset($defaultUrls[$key])) {
-					$settings[$key]['url'] = NetCommonsUrl::actionUrl($defaultUrls[$key]);
-				}
-			}
-		}
-		$controller->set('settingTabs', $settings);
+			$settings[$key] = $this->settings['mainTabs'][$key];
 
+			if (! isset($defaultUrls[$key])) {
+				continue;
+			}
+
+			if (isset($this->settings['mainTabs'][$key]['url']['plugin'])) {
+				$defaultUrls[$key]['plugin'] = $this->settings['mainTabs'][$key]['url']['plugin'];
+			}
+			if (isset($this->settings['mainTabs'][$key]['url']['controller'])) {
+				$defaultUrls[$key]['controller'] = $this->settings['mainTabs'][$key]['url']['controller'];
+			}
+			if (isset($this->settings['mainTabs'][$key]['url']['action'])) {
+				$defaultUrls[$key]['action'] = $this->settings['mainTabs'][$key]['url']['action'];
+			}
+
+			$settings[$key]['url'] = NetCommonsUrl::actionUrl($defaultUrls[$key]);
+		}
+
+		$controller->set('settingTabs', $settings);
+	}
+
+/**
+ * Set block tabs
+ *
+ * @param Controller $controller Controller with components to beforeRender
+ * @return void
+ */
+	private function __setBlockTabs(Controller $controller) {
 		//ブロック設定のタブ
 		$defaultUrls = array(
 			self::BLOCK_TAB_SETTING => array(
@@ -100,24 +146,41 @@ class BlockTabsComponent extends Component {
 				'frame_id' => Current::read('Frame.id'),
 				'block_id' => Current::read('Block.id'),
 			),
+			self::BLOCK_TAB_PERMISSION => array(
+				'plugin' => $controller->params['plugin'],
+				'controller' => Inflector::singularize($controller->params['plugin']) . '_block_role_permissions',
+				'action' => 'edit',
+				'frame_id' => Current::read('Frame.id'),
+				'block_id' => Current::read('Block.id'),
+			),
 		);
 
 		$settings = array();
 		foreach ($this->settings['blockTabs'] as $key => $tab) {
-			if (in_array($tab, $this->settings['blockTabs'])) {
+			if (! is_array($tab) && in_array($tab, $this->settings['blockTabs'], true)) {
 				$this->settings['blockTabs'][$tab] = array();
 				$key = $tab;
 			}
 
-			if (isset($this->settings['blockTabs'][$key])) {
-				$settings[$key] = $this->settings['blockTabs'][$key];
-				if (! isset($this->settings['blockTabs'][$key]['url']) && isset($defaultUrls[$key])) {
-					$settings[$key]['url'] = NetCommonsUrl::actionUrl($defaultUrls[$key]);
-				}
+			$settings[$key] = $this->settings['blockTabs'][$key];
+
+			if (! isset($defaultUrls[$key])) {
+				continue;
 			}
+
+			if (isset($this->settings['blockTabs'][$key]['url']['plugin'])) {
+				$defaultUrls[$key]['plugin'] = $this->settings['blockTabs'][$key]['url']['plugin'];
+			}
+			if (isset($this->settings['blockTabs'][$key]['url']['controller'])) {
+				$defaultUrls[$key]['controller'] = $this->settings['blockTabs'][$key]['url']['controller'];
+			}
+			if (isset($this->settings['blockTabs'][$key]['url']['action'])) {
+				$defaultUrls[$key]['action'] = $this->settings['blockTabs'][$key]['url']['action'];
+			}
+
+			$settings[$key]['url'] = NetCommonsUrl::actionUrl($defaultUrls[$key]);
 		}
 		$controller->set('blockSettingTabs', $settings);
-
-		$this->controler = $controller;
 	}
+
 }
