15d148a5490ecd1741dc5bccfef67ade92a7a643
s-nakajima <nakajimashouhei@gmail.com>
2016-03-09 16:00:43 +0900

    BlockRolePermissionsControllerEditTest::testAccessPermission()のテスト

---
+++ Test/Case/TestSuite/BlockRolePermissionsControllerEditTest/TestAccessPermissionTest.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * BlockRolePermissionsControllerEditTest::testAccessPermission()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
+
+/**
+ * BlockRolePermissionsControllerEditTest::testAccessPermission()のテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Blocks\Test\Case\TestSuite\BlockRolePermissionsControllerEditTest
+ */
+class TestSuiteBlockRolePermissionsControllerEditTestTestAccessPermissionTest extends NetCommonsControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array();
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'blocks';
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		//テストプラグインのロード
+		NetCommonsCakeTestCase::loadTestPlugin($this, 'Blocks', 'TestBlocks');
+		App::uses('TestSuiteBlockRolePermissionsControllerEditTest', 'TestBlocks.TestSuite');
+		$this->TestSuite = new TestSuiteBlockRolePermissionsControllerEditTest();
+	}
+
+/**
+ * testAccessPermission()のテスト
+ *
+ * @return void
+ */
+	public function testTestAccessPermission() {
+		//データ生成
+		$role = 'general_user';
+		$exception = null;
+
+		//テスト実施
+		$result = $this->TestSuite->testAccessPermission($role, $exception);
+
+		//チェック
+		$pattern = '/' . preg_quote('TestSuite/BlockRolePermissionsControllerEditTest/edit.ctp', '/') . '/';
+		$this->assertRegExp($pattern, $result->view);
+		$this->assertEquals('general_user', $result->controller->viewVars['username']);
+	}
+
+/**
+ * testAccessPermission()のテスト(ログインなし)
+ *
+ * @return void
+ */
+	public function testTestAccessPermissionWOLogin() {
+		//データ生成
+		$role = null;
+		$exception = null;
+
+		//テスト実施
+		$result = $this->TestSuite->testAccessPermission($role, $exception);
+
+		//チェック
+		$pattern = '/' . preg_quote('TestSuite/BlockRolePermissionsControllerEditTest/edit.ctp', '/') . '/';
+		$this->assertRegExp($pattern, $result->view);
+		$this->assertNull($result->controller->viewVars['username']);
+	}
+
+}
