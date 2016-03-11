18bb61b975b61d7072c5299762fe1d0848bc31e0
s-nakajima <nakajimashouhei@gmail.com>
2016-03-11 21:51:12 +0900

    View/Elements/delete_formのテスト

--- Test/Case/View/Elements/DeleteFormTest.php
+++ Test/Case/View/Elements/DeleteFormTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * View/Elements/edit_formのテスト
+ * View/Elements/delete_formのテスト
  *
  * @author Noriko Arai <arai@nii.ac.jp>
  * @author Shohei Nakajima <nakajimashouhei@gmail.com>
@@ -12,12 +12,12 @@
 App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
 
 /**
- * View/Elements/edit_formのテスト
+ * View/Elements/delete_formのテスト
  *
  * @author Shohei Nakajima <nakajimashouhei@gmail.com>
- * @package NetCommons\Blocks\Test\Case\View\Elements\EditForm
+ * @package NetCommons\Blocks\Test\Case\View\Elements\DeleteForm
  */
-class BlocksViewElementsEditFormTest extends NetCommonsControllerTestCase {
+class BlocksViewElementsDeleteFormTest extends NetCommonsControllerTestCase {
 
 /**
  * Fixtures
@@ -44,28 +44,28 @@ class BlocksViewElementsEditFormTest extends NetCommonsControllerTestCase {
 		//テストプラグインのロード
 		NetCommonsCakeTestCase::loadTestPlugin($this, 'Blocks', 'TestBlocks');
 		//テストコントローラ生成
-		$this->generateNc('TestBlocks.TestViewElementsEditForm');
+		$this->generateNc('TestBlocks.TestViewElementsDeleteForm');
 	}
 
 /**
- * View/Elements/edit_formのテスト
+ * View/Elements/delete_formのテスト
  *
  * @return void
  */
-	public function testEditForm() {
+	public function testDeleteForm() {
 		//テスト実行
-		$this->_testGetAction('/test_blocks/test_view_elements_edit_form/edit_form',
+		$this->_testGetAction('/test_blocks/test_view_elements_delete_form/delete_form',
 				array('method' => 'assertNotEmpty'), null, 'view');
 
 		//チェック
-		$pattern = '/' . preg_quote('View/Elements/edit_form', '/') . '/';
+		$pattern = '/' . preg_quote('View/Elements/delete_form', '/') . '/';
 		$this->assertRegExp($pattern, $this->view);
 
-		$pattern = '/' . preg_quote('View/Elements/test_view_elements_edit_form', '/') . '/';
+		$pattern = '/' . preg_quote('View/Elements/test_view_elements_delete_form', '/') . '/';
 		$this->assertRegExp($pattern, $this->view);
 
-		$this->assertInput('form', null, '/test_blocks/test_ctrl/edit', $this->view);
-		$this->assertInput('input', '_method', 'PUT', $this->view);
+		$this->assertInput('form', null, '/test_blocks/test_ctrl/delete', $this->view);
+		$this->assertInput('input', '_method', 'DELETE', $this->view);
 	}
 
 }
--- Test/test_app/Plugin/TestBlocks/Controller/TestViewElementsDeleteFormController.php
+++ Test/test_app/Plugin/TestBlocks/Controller/TestViewElementsDeleteFormController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * View/Elements/edit_formテスト用Controller
+ * View/Elements/delete_formテスト用Controller
  *
  * @author Noriko Arai <arai@nii.ac.jp>
  * @author Shohei Nakajima <nakajimashouhei@gmail.com>
@@ -12,36 +12,25 @@
 App::uses('AppController', 'Controller');
 
 /**
- * View/Elements/edit_formテスト用Controller
+ * View/Elements/delete_formテスト用Controller
  *
  * @author Shohei Nakajima <nakajimashouhei@gmail.com>
  * @package NetCommons\Blocks\Test\test_app\Plugin\TestBlocks\Controller
  */
-class TestViewElementsEditFormController extends AppController {
+class TestViewElementsDeleteFormController extends AppController {
 
 /**
- * use models
- *
- * @var array
- */
-	public $uses = array(
-		'TestBlocks.TestViewElementsEditFormModel',
-	);
-
-/**
- * edit_form
+ * delete_form
  *
  * @return void
  */
-	public function edit_form() {
+	public function delete_form() {
 		$this->autoRender = true;
 		$this->set('viewVars', array(
 			'model' => 'TestViewElementsEditFormModel',
-			'callback' => 'TestBlocks.test_view_elements_edit_form',
-			'action' => '/test_blocks/test_ctrl/edit'
+			'callback' => 'TestBlocks.test_view_elements_delete_form',
+			'action' => '/test_blocks/test_ctrl/delete'
 		));
-
-		$this->request->data['TestViewElementsEditFormModel'] = array('id' => '1');
 	}
 
 }
