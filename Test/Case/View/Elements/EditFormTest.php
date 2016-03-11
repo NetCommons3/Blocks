1a15bb8073ffaf4dea90d032ac074526bd9a71d9
s-nakajima <nakajimashouhei@gmail.com>
2016-03-11 21:35:39 +0900

    View/Elements/edit_formのテスト

---
+++ Test/Case/View/Elements/EditFormTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * View/Elements/edit_formのテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
+
+/**
+ * View/Elements/edit_formのテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Blocks\Test\Case\View\Elements\EditForm
+ */
+class BlocksViewElementsEditFormTest extends NetCommonsControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array();
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'blocks';
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		//テストプラグインのロード
+		NetCommonsCakeTestCase::loadTestPlugin($this, 'Blocks', 'TestBlocks');
+		//テストコントローラ生成
+		$this->generateNc('TestBlocks.TestViewElementsEditForm');
+	}
+
+/**
+ * View/Elements/edit_formのテスト
+ *
+ * @return void
+ */
+	public function testEditForm() {
+		//テスト実行
+		$this->_testGetAction('/test_blocks/test_view_elements_edit_form/edit_form',
+				array('method' => 'assertNotEmpty'), null, 'view');
+
+		//チェック
+		$pattern = '/' . preg_quote('View/Elements/edit_form', '/') . '/';
+		$this->assertRegExp($pattern, $this->view);
+
+		$pattern = '/' . preg_quote('View/Elements/test_view_elements_edit_form', '/') . '/';
+		$this->assertRegExp($pattern, $this->view);
+
+		$this->assertInput('form', null, '/test_blocks/test_view_elements_edit_form/edit', $this->view);
+		$this->assertInput('input', '_method', 'PUT', $this->view);
+	}
+
+}
