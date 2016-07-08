48021f9606d888f802ff0af266b2180497c284b9
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-07-08 18:04:19 +0900

    BlockSettingMigration追加

---
+++ Config/Migration/BlockSettingMigration.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * BlockSettingMigration
+ *
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsMigration', 'NetCommons.Config/Migration');
+
+/**
+ * BlockSettingMigration
+ *
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @package NetCommons\Mails\Config\Migration
+ */
+class BlockSettingMigration extends NetCommonsMigration {
+
+/**
+ * マイグレーションupの更新と,downの削除
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @param string $pluginKey プラグインキー
+ * @return bool Should process continue
+ */
+	public function updateAndDelete($direction, $pluginKey) {
+		$this->loadModels(array(
+			'BlockSetting' => 'Blocks.BlockSetting',
+		));
+
+		foreach ($this->records as $model => $records) {
+			$conditions = array(
+				'plugin_key' => $pluginKey,
+				'room_id' => null,
+				'block_key' => null,
+			);
+
+			if ($direction == 'up') {
+				if (!$this->updateRecords($model, $records)) {
+					return false;
+				}
+
+			} elseif ($direction == 'down') {
+				if (!$this->BlockSetting->deleteAll($conditions, false, false)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
