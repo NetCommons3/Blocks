a29b25aae8d768b1020e6e3f840771198922aea4
s-nakajima <nakajimashouhei@gmail.com>
2016-08-21 02:16:59 +0900

    ブロックの状態をブロックタイトルの横に付与する https://github.com/NetCommons3/NetCommons3/issues/397

---
+++ View/Helper/BlocksHelper.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * BlocksHelper
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('AppHelper', 'View/Helper');
+App::uses('Block', 'Blocks.Model');
+
+/**
+ * BlocksHelper
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Blocks\View\Helper
+ */
+class BlocksHelper extends AppHelper {
+
+/**
+ * 使用ヘルパー
+ *
+ * @var array
+ */
+	public $helpers = array(
+		'Html',
+		'NetCommons.NetCommonsHtml',
+	);
+
+/**
+ * Before render callback. beforeRender is called before the view file is rendered.
+ *
+ * Overridden in subclasses.
+ *
+ * @param string $viewFile The view file that is going to be rendered
+ * @return void
+ */
+	public function beforeRender($viewFile) {
+		$this->NetCommonsHtml->css('/blocks/css/style.css');
+		parent::beforeRender($viewFile);
+	}
+
+/**
+ * タイトル（ブロックタイトル）の出力
+ *
+ * #### サンプル
+ * ```
+ * echo $this->NetCommonsHtml->blockTitle($bbs['name'])
+ * ```
+ * ##### 出力結果
+ * ```
+ * <h1>新しい掲示板 20160513074815</h1>
+ * ```
+ *
+ * @param string $text タイトル
+ * @param string $titleIcon タイトルアイコン
+ * @param array $options HTML属性オプション
+ * @return string `<h1>`タグ
+ */
+	public function blockTitle($text = '', $titleIcon = null, $options = array()) {
+		$output = '';
+
+		$escape = Hash::get($options, 'escape', true);
+		if ($escape) {
+			$text = h($text);
+		}
+		$options = Hash::insert($options, 'escape', false);
+
+		if ($titleIcon) {
+			$text = $this->NetCommonsHtml->titleIcon($titleIcon) . ' ' . $text;
+		}
+
+		$text =  $this->getBlockStatus(true) . ' ' . $text;
+
+		$output .= $this->Html->tag('h1', $text, $options);
+		return $output;
+	}
+
+/**
+ * ブロックのステータスラベルを表示
+ *
+ * @return string HTML
+ */
+	public function getBlockStatus($isSetting = null) {
+		$html = '';
+
+		if (! isset($isSetting)) {
+			$isSetting = Current::isSettingMode();
+		}
+
+		if (! $isSetting || ! Current::read('Block.id')) {
+			return $html;
+		}
+
+		$block = Current::read('Block', array());
+
+		$publicType = Hash::get($block, 'public_type');
+		if ($publicType === Block::TYPE_PUBLIC) {
+			return $html;
+		}
+
+		$now = date('Y-m-d H:i:s');
+		$html .= '<span class="block-style-label label label-default">';
+
+		if ($publicType === Block::TYPE_PRIVATE) {
+			$html .= __d('blocks', 'Private');
+		} elseif ($publicType === Block::TYPE_LIMITED) {
+			if ($now < Hash::get($block, 'publish_start')) {
+				$html .= __d('blocks', 'Public before');
+			} elseif ($now > Hash::get($block, 'publish_end')) {
+				$html .= __d('blocks', 'Public end');
+			} else {
+				$html .= __d('blocks', 'Limited');
+			}
+		}
+
+		$html .= '</span>';
+
+		return $html;
+	}
+
+}
