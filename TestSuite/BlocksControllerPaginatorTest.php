2fb011ae88ab594fb633553186b0161cfd9d2ef2
s-nakajima <nakajimashouhei@gmail.com>
2017-08-26 15:53:04 +0900

    phpunit4以降でabstract classにする必要ある https://github.com/NetCommons3/NetCommons3/issues/998

--- TestSuite/BlocksControllerPaginatorTest.php
+++ TestSuite/BlocksControllerPaginatorTest.php
@@ -18,7 +18,7 @@ App::uses('WorkflowComponent', 'Workflow.Controller/Component');
  * @author Shohei Nakajima <nakajimashouhei@gmail.com>
  * @package NetCommons\Blocks\TestSuite
  */
-class BlocksControllerPaginatorTest extends NetCommonsControllerTestCase {
+abstract class BlocksControllerPaginatorTest extends NetCommonsControllerTestCase {
 
 /**
  * Edit controller name
